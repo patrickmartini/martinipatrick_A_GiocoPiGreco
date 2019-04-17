a032f576b87fa0fab4832f37d29bdcf607bcb0aa
Patrick Martini <patrick.martini@barsanti.edu.it>
2019-04-17 13:40:00 +0200

    1a  Versione
    Preparato la cartella di GitKraken e scritto do robe

---
+++ PiGreco/PiGreco.ino
@@ -0,0 +1,17 @@
+#include <LiquidCrystal.h>
+
+int button1 = 2;
+int button2 = 3;
+int button3 = 4;
+int button4 = 5;
+int button5 = 6;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
